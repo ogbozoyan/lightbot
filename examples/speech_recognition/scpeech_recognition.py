99fbae89177fb80e884041b2a9ab99e8479cd16f
Dmitrii Oreshkin <71903279+dmitrii-subb@users.noreply.github.com>
2022-08-31 11:53:14 +0300

    Create scpeech_recognition.py

---
+++ examples/speech_recognition/scpeech_recognition.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+import subprocess
+import ffmpeg
+import os
+from lightbot import bot
+
+
+def on_voice():
+
+    bot.download_file(bot.voice['file_id'], './voice.opus')
+
+    process = subprocess.run(
+        ['./ffmpeg', '-i', './voice.opus', './voice.wav'],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT
+    )
+
+    if not process.returncode:
+        r = sr.Recognizer()
+        voice = sr.AudioFile('voice.wav')
+        with voice as source:
+            audio = r.record(source)
+            recognized_data = r.recognize_google(audio, language='ru', show_all = True)
+            if recognized_data:
+                recognized_text = recognized_data['alternative'][0]['transcript']
+            else:
+                recognized_text = 'None'
+
+        bot.send_message(recognized_text)
+
+        os.remove('./voice.wav')
+        os.remove('./voice.opus')
+
+
+if __name__ == '__main__':
+    bot.bind_event('voice', on_voice)
+    bot.run(token="5595021127:AAH9JTi2pt8UZutLSnIUhx7u2tzlU6YoxWc", show_event=True)
